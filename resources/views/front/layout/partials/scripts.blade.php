a5a1c9f1865008cdee1d8c999b89368ae3ce6f9c
b4llesteros <b4llesteros@gmail.com>
2022-06-06 20:18:16 +0200

    Font Script Added

--- resources/views/front/layout/partials/scripts.blade.php
+++ resources/views/front/layout/partials/scripts.blade.php
@@ -5,3 +5,12 @@
 @if($agent->isMobile())
     <script src="{{mix('front/mobile/js/app-mobile.js')}}"></script>
 @endif
+
+        <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+          <script>
+                WebFont.load({
+                    google: {
+                        families: ['Noto Sans']
+                    }
+                });        
+        </script>
